3fbe5f790f64b2e0955c911787770bc049cc8270
ecastaneda <ecastaneda@bec.seguridad.unam.mx>
2015-05-10 15:00:39 -0500

    Mas cosas

--- Mod5.py
+++ Mod5.py
@@ -5,13 +5,12 @@ import datas
 import base64
 
 # Access to Database
-host=base64.b64decode(Hola.tsoh)[:-1]
-user=base64.b64decode(Hola.resu)[:-1]
-pwd=base64.b64decode(Hola.dwp)[:-1]
-dbname=base64.b64decode(Hola.emanbd)[:-1]
+host=base64.b64decode(datas.tsoh)[:-1]
+user=base64.b64decode(datas.resu)[:-1]
+pwd=base64.b64decode(datas.dwp)[:-1]
+dbname=base64.b64decode(datas.emanbd)[:-1]
 
 class Postgress:
-	conn = ""
 	def __init__(self):
 		try:
 			credentials ="dbname={0} user={1} host={2} password={3}".format(dbname,user,host,pwd)
@@ -21,21 +20,27 @@ class Postgress:
 			#self.conn.close()
 		except:
 			print "Sorry, unable to connect to PostgreSQL server\ncheck your configurations"
-	def version(self):
-		try:
-			cursor = self.conn.cursor()
-			print "Version"
-			cursor.execute("select * from tabla")
-			vers = cursor.fetchall()
-			print vers
-		except:
-			print "Sorry, unable to connect execute sentence"
 	def __del__(self):
 		try:
 			self.conn.close()
 		except:
 			print "Sorry, unable to disconnect from database"
-
+	def version(self):
+		try:
+			cursor = self.conn.cursor()
+			cursor.execute("select version()")
+			vers = cursor.fetchall()
+			print vers
+		except:
+			print "Sorry, unable to connect execute sentence"
+	def Cosecha(self):
+		#Metodo para la recoleccion de datos que necesita el reporte
+		try:
+			cursor = self.conn.cursor()
+			cursor.execute("select ")
+			print "Cosecha"
+		except:
+			print "Sorry, An error has ocurred"
 if __name__ =="__main__":
 	#print host
 	repe = Postgress()
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Proyecto-3
 Comprende el Modulo 5 y 6
+# Requirements
+psycopg2 - PostgreSQL Connector
+iwkhtmltopdf - PDFCreator from HTML
+#Install Requirements
+Debian
+	apt-get install wkhtmltopdf psycopg2
+RedHat
+	yum install wkhtmltopdf psycopg2
+or
+You must have installed python-pip and then execute
+	pip install psycopg2
