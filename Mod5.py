a8e5642c83b1247492be3b10e078138b10b6eaf1
ecastaneda <ecastaneda@bec.seguridad.unam.mx>
2015-05-11 13:49:06 -0500

    POO

--- Mod5.py
+++ Mod5.py
@@ -1,56 +1,18 @@
 #!/usr/bin/env python
 import time
-import psycopg2
-import datas
-import base64
+from Reportes import Reportes
+from PostgreSQL import Postgress
 
-# Access to Database
-host=base64.b64decode(datas.tsoh)[:-1]
-user=base64.b64decode(datas.resu)[:-1]
-pwd=base64.b64decode(datas.dwp)[:-1]
-dbname=base64.b64decode(datas.emanbd)[:-1]
-
-class Postgress:
-	def __init__(self):
-		try:
-			credentials ="dbname={0} user={1} host={2} password={3}".format(dbname,user,host,pwd)
-			print credentials[:-20]
-			self.conn=psycopg2.connect(credentials)
-			print "Successfull"
-			#self.conn.close()
-		except:
-			print "Sorry, unable to connect to PostgreSQL server\ncheck your configurations"
-	def __del__(self):
-		try:
-			self.conn.close()
-		except:
-			print "Sorry, unable to disconnect from database"
-	def version(self):
-		try:
-			cursor = self.conn.cursor()
-			cursor.execute("select version()")
-			vers = cursor.fetchall()
-			print vers
-		except:
-			print "Sorry, unable to connect execute sentence"
-	def Cosecha(self):
-		#Metodo para la recoleccion de datos que necesita el reporte
-		try:
-			cursor = self.conn.cursor()
-			cursor.execute("select * from datos_phishing")
-			datos_pishing = cursor.fetchall()
-			#Ahora se tiene que acomodar los datos en el reporte HTML
-			#print type(datos_pishing)
-			for id in range(len(datos_pishing)):
-				for e in datos_pishing[id]:
-					print e
-		except:
-			print "Sorry, An error has ocurred in Cosecha"
+dPishing = ''
 
 def main():
-	#repe = Postgress()
-	#repe.Cosecha()
+	#Se conecta a la base de datos
+	repe = Postgress()
+	#Obtiene los registros de la tabla de los datos phishing
+	dPishing = repe.Get_dPhishing()
+	#print dPishing
 	generar = Reportes()
+	generar.Reporte_Incidentes(dPishing)
 
 if __name__ =="__main__":
 	main()
--- Reportes.py
+++ Reportes.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
+from time import sleep, strftime
+
 class Reportes:
-	csirt = False
-	def __init__(self,csirt=False):
-		if(csirt):
-			print "Generar Reporte para el Equipo de Respuesta a incidentes"
-		else:
-			print "Generar reporte para los administradores"
-	def Reporte_Incidentes(self):
+	pathTemplate =''
+	def __init__(self):
+		self.pathTemplate="./salcilocos/"
+	def __del__(self):
+		self.pathTemplate=''
+	def Reporte_Incidentes(self,datosPhishing):
 		try:
-			
+			print "Generar Reporte para el Equipo de Respuesta a incidentes"
+			sleep(2)
+			archivoRI = open(self.pathTemplate+"template_csirt.html","r").read()
+			#print type(archivoRI)
+			archivoRI = archivoRI.format(datosPhishing,1,2,3,4,5,6,7,8)
+			fiile = self.pathTemplate+"ReportResponseTeam-"+strftime("%Y-%m-%d-%S")+".html"
+			print type(fiile)
+			#Genera el reporte en HTML
+			rIncidentes = open(fiile,"w")
+			rIncidentes.write(archivoRI)
+			rIncidentes.close()
 		except:
-			print "No fue posible generar el reporte para el equipo de Respjuesta a Incidentes"
+			print "No fue posible generar el reporte para el equipo de Respuesta a Incidentes"
 	def Reporte_Admins(self):
 		try:
-			
+			print "Generar reporte para los administradores"
+			sleep(2)
+			aAdmin = open(self.pathTemplate+"template_admin.html","r").read()
+			adminFile = open()
+			print adminFile
 		except:
 			print "No fue posible generar el reporte para los administradores"
